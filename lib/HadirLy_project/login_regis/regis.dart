256910e6b026f606d040cc1cea94c4c9583442c0
qeisandi <qeis.andi02@gmail.com>
2025-07-07 11:04:27 +0700

    first commit

---
+++ lib/HadirLy_project/login_regis/regis.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Regis extends StatefulWidget {
+  const Regis({super.key});
+
+  @override
+  State<Regis> createState() => _RegisState();
+}
+
+class _RegisState extends State<Regis> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
